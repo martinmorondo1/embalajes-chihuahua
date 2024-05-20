71c675d24762c2e14f5eb03150a9d934ba90fbed
martinmorondo1 <128125058+martinmorondo1@users.noreply.github.com>
2024-05-20 19:45:56 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,96 +1,38 @@
 (function ($) {
     "use strict";
-    
-    // Dropdown on mouse hover
+
+    // Función para activar el carrusel personalizado
     $(document).ready(function () {
-        function toggleNavbarMethod() {
-            if ($(window).width() > 992) {
-                $('.navbar .dropdown').on('mouseover', function () {
-                    $('.dropdown-toggle', this).trigger('click');
-                }).on('mouseout', function () {
-                    $('.dropdown-toggle', this).trigger('click').blur();
-                });
-            } else {
-                $('.navbar .dropdown').off('mouseover').off('mouseout');
-            }
-        }
-        toggleNavbarMethod();
-        $(window).resize(toggleNavbarMethod);
-    });
-    
-    
-    // Back to top button
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {
-            $('.back-to-top').fadeIn('slow');
-        } else {
-            $('.back-to-top').fadeOut('slow');
-        }
-    });
-    $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
-        return false;
-    });
-
-
-    // Facts counter
-    $('[data-toggle="counter-up"]').counterUp({
-        delay: 10,
-        time: 2000
-    });
-
-
-    // Modal Video
-    $(document).ready(function () {
-        var $videoSrc;
-        $('.btn-play').click(function () {
-            $videoSrc = $(this).data("src");
+        $(".custom-carousel").owlCarousel({
+            autoWidth: true,
+            loop: true
         });
-        console.log($videoSrc);
 
-        $('#videoModal').on('shown.bs.modal', function (e) {
-            $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
-        })
-
-        $('#videoModal').on('hide.bs.modal', function (e) {
-            $("#video").attr('src', $videoSrc);
-        })
+        // Evento click para marcar elemento activo
+        $(".custom-carousel .item").click(function () {
+            $(".custom-carousel .item").not($(this)).removeClass("active");
+            $(this).toggleClass("active");
+        });
     });
 
+    // Función para controlar la navegación del carrusel con botones de prev y next
+    $(document).ready(function () {
+        // Selector del carrusel
+        var carousel = $(".custom-carousel");
 
-    // Testimonials carousel
-    $(".testimonial-carousel").owlCarousel({
-        center: true,
-        autoplay: true,
-        smartSpeed: 1500,
-        margin: 30,
-        dots: true,
-        loop: true,
-        responsive: {
-            0:{
-                items:1
-            },
-            576:{
-                items:1
-            },
-            768:{
-                items:2
-            },
-            992:{
-                items:3
-            }
-        }
+        // Selector de los botones de prev y next
+        var prevButton = $(".custom-nav .arrow.prev");
+        var nextButton = $(".custom-nav .arrow.next");
+
+        // Evento click para el botón de prev
+        prevButton.click(function () {
+            carousel.trigger("prev.owl.carousel");
+        });
+
+        // Evento click para el botón de next
+        nextButton.click(function () {
+            carousel.trigger("next.owl.carousel");
+        });
     });
-    
+
 })(jQuery);
-
-$(".custom-carousel").owlCarousel({
-    autoWidth: true,
-    loop: true
-  });
-  $(document).ready(function () {
-    $(".custom-carousel .item").click(function () {
-      $(".custom-carousel .item").not($(this)).removeClass("active");
-      $(this).toggleClass("active");
-    });
-  });
